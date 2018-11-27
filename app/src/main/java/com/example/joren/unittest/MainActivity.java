4cca9f5c391e6f8a87b91da2b5e41d6e70a3cecc
Joren Jamar <jorenjamar@hotmail.com>
2018-11-27 11:38:51 +0100

    Second commit

--- app/src/main/java/com/example/joren/unittest/MainActivity.java
+++ app/src/main/java/com/example/joren/unittest/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
     }
 }
